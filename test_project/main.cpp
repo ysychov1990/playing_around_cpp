e5b59060cadfc460db07e85da476384c8ee62b38
ysychov1990@gmail.com <ysychov1990@gmail.com>
2025-06-22 10:52:29 +0200

    another commit before tree merge

--- test_project/main.cpp
+++ test_project/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    cout << "Hello World!" << endl;
+    int liczba;
+    cout << "Wprowadź swoją ulubioną liczbę od 1 do 100: ";
+    cin >> liczba;
+    cout << "Co za przypadek! " << liczba << " to jest moja ulubiona liczba też!" << endl;
+//    cout << "Hello World!" << endl;
 	return 0;
 }
